df660ee1053ee5ab69778c85278d06c365b81216
UKISTU18 <arajegan.aj@gmail.com>
2019-03-25 11:34:17 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* eslint-env browser */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game';
+import './index.css';
+
+ReactDOM.render(<Game />, document.getElementById('root'));
